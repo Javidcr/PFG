4162c2ffbda3a2abc6c765678da2589bb8498f60
javidcr <javicastillo92@gmail.com>
2017-06-05 17:24:57 +0200

    Cambios en arp spoofing defense

--- ARP_spoofing/arp_defense.py
+++ ARP_spoofing/arp_defense.py
@@ -6,8 +6,34 @@ from scapy.all import ARP, sniff
 import tkMessageBox
 import commands
 import os
+import nmap
+import time
 
-diccionario = dict() # diccionario
+diccionario = dict() # diccionario para almacenar IP y MAC de los pcs.
+
+def analizar_red():
+    #We create a nm object that will be used to scan the network
+    nm = nmap.PortScanner()
+    #We give the scan parameter, and perform the scan
+    nm.scan(hosts = '192.168.1.0/24', arguments = '-n -sP -PE -T5')
+
+    #Recording local time of the scan in the object localtime using a function from the time package
+    localtime = time.asctime(time.localtime(time.time()))
+    #Printing time on screen
+    print('============ {0} ============'.format(localtime))
+
+    #Since we performed the scan, the function of nm, all_hosts() can be used to get the result of the scan as a list
+    #We go through each element of the list
+    for host in nm.all_hosts():
+        # If the status of an element is not down, we print it
+        if nm[host]['status']['state'] != "down":
+            print 
+            print "IP ADDRESS:", host
+            print "STATUS:", nm[host]['status']['state']
+            try:
+                print "MAC ADDRESS:", nm[host]['addresses']['mac']
+            except:
+                mac = 'unknown'
 
 def mostrar_resultado(resultado):
     #si no hay errores
@@ -68,6 +94,7 @@ def parar_ejecucion():
 
 if __name__ == '__main__':
     try:
+        analizar_red()
         while 1:
             sniff(prn=analizar_paquetes, filter="arp")
 
