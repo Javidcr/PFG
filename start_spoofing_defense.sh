a9991cea212741b86916909b611c7aed24f27c75
Fco Javier del Castillo <javicastillo92@gmail.com>
2018-01-16 18:07:25 +0100

    Limpieza en repositorio

--- start_spoofing_defense.sh
+++ start_spoofing_defense.sh
@@ -8,10 +8,28 @@ echo -e '\t[ '$DIA'  | '$HORA' ]\r'
 echo -e '\tIniciando aplicacion para la defenda de ataques spoofing...'
 
 echo -e '[#####                  ](33%)\r'
-./start_ip_defense.sh
+
+#Arranco IP defense
+cd IP_spoofing
+gnome-terminal -x bash -c "python ip_defense.py" &
+PID_IP=$!
+cd ..
+echo -n 'Arrancado proceso ip_defense con PID = '
+echo  $PID_IP | tee pid_ip
+#Fin Arranco IP defense
 
 echo -e '[#############          ](66%)\r'
-./start_arp_defense.sh
+
+#Arranco ARP defense
+cd ARP_spoofing
+gnome-terminal -x bash -c "python arp_defense.py" &
+PID_ARP=$!
+cd ..
+echo -n 'Arrancado proceso arp_defense con PID = '
+echo  $PID_ARP | tee pid_arp
+#Fin Arranco ARP defense
 
 echo -e '[#######################](100%)\r'
 echo -e '\n'
+
+exit 0
